1bdff61c9347dd7fedfe1b0ed7f60dd2b2060200
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-06 23:53:54 +0530

    Add files via upload

---
+++ Basic/addition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class addition{
+public static void main(String args[]){
+int a,b;
+Scanner sc = new Scanner(System.in);
+System.out.println("enter 1st number:");
+a=sc.nextInt();
+System.out.println("enter 2nd number:");
+b=sc.nextInt();
+int c;
+c=a+b;
+System.out.println("sum is:"+c);
+}
+}
