9ff6cf514e4df42984b7ddfb3c98ddef30d684e5
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-11 23:02:17 +0530

    Add files via upload

---
+++ Basic/triangle.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class atriangle {
+
+float b,h,l;
+Scanner ip = new Scanner(System.in);
+
+void readdata() {
+
+System.out.print("enter base: ");
+b = ip.nextFloat();
+
+
+System.out.print("enter height: ");
+h = ip.nextFloat();
+
+
+System.out.print("enter length: ");
+l = ip.nextFloat();
+
+}
+
+void area(){
+
+public static void main(String[] args){
+
+float sp = (b+h+l)/3;
+float result = sp*(sp-b)*(sp-h)*(sp-l);
+
+float area = Math.sqrt(result);
+
+System.out.print("Area of Triangle: "+area);
+}
+
+}
+
+
+
+
+
+
+
+
+
+
