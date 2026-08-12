d9a7fb296e5adb79afcbb9fb3146de40e4a889d3
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-12 18:58:43 +0530

    Add files via upload

---
+++ Basic/box.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class box {
+int l,b,h;
+
+void readdata(){
+Scanner ip = new Scanner(System.in);
+
+System.out.println("enter length of box:");
+l = ip.nextInt();
+
+System.out.println("enter breadth of box:");
+b = ip.nextInt();
+
+System.out.println("enter height of box:");
+h = ip.nextInt();
+
+System.out.printf("length : %d\nbreadth : %d\nheight : %d\n\n",l,b,h);
+}
+
+void area() {
+
+System.out.println("Area = "+(2*((l*b)+(b*h)+(h*l))));
+}
+
+void volume(){
+
+System.out.println("Area = "+(l*b*h));
+}
+
+public static void main(String args[]){
+
+box ob = new box();
+ob.readdata();
+ob.volume();
+ob.area();
+}
+}
+
+
+
+
+
+
+
+
