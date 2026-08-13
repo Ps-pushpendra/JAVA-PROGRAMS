e3cd00d62a94e430cffa198511198b7aad4534a0
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-13 23:39:08 +0530

    Add files via upload

---
+++ Basic/conditions.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class conditions {
+
+public static void main( String[] args ){
+
+Scanner ip = new Scanner(System.in);
+float a,b;
+
+System.out.print("enter a:");
+a = ip.nextFloat();
+
+System.out.print("enter b:");
+b = ip.nextFloat();
+
+if(a==b) System.out.println("Both a&b are Equal");
+
+else if(a>b)  System.out.println("a is greater");
+
+else  System.out.println("b is greater");
+
+}
+}
+
+
+
