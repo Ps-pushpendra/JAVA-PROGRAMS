0f4a45b7efae5799c99eedb3d4c0aef3e538abf1
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-14 22:48:01 +0530

    Update top.java

--- Loops/top.java
+++ Loops/top.java
@@ -1,4 +1,6 @@
-class top {
+// program for initialize / declare / displaying array
+
+class array {
 
 public static void main(String[] args) {
 
@@ -7,8 +9,9 @@ int a[] = new int[5];
 for(int i=0;i<a.length;i++) a[i] = i*2;
 System.out.println("Display Array:");
 
-// for (int i=0;i<a.length;i++) System.out.println(a[i]);
+for (int i=0;i<a.length;i++) System.out.println(a[i]);
 for(int i:a)
 System.out.println(i);
 
-}}
+}
+}
