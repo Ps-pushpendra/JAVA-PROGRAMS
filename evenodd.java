529474651b7ac47cfd7c140a5805e7235cfa8c22
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-16 23:09:28 +0530

    Update evenodd.java

--- evenodd.java
+++ evenodd.java
@@ -1,13 +1,20 @@
+// program to check a number is even or odd.
+
 import java.util.Scanner;
 
 class evenodd{
+  
 public static void main(String args[]){
+  
 Scanner ip = new Scanner(System.in);
 int n;
+  
 System.out.println("enter number:");
 n=ip.nextInt();
+  
 if(n%2==0)
 System.out.println("Even number..");
+  
 else
 System.out.println("Odd number..");
 }
