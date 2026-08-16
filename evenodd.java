833f5fce95dca36cc9756dee52f3b08e41c92946
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-08-16 23:08:10 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class evenodd{
+public static void main(String args[]){
+Scanner ip = new Scanner(System.in);
+int n;
+System.out.println("enter number:");
+n=ip.nextInt();
+if(n%2==0)
+System.out.println("Even number..");
+else
+System.out.println("Odd number..");
+}
+}
+
